8d4f21ceb878e4ad5189e303952c510ff8851d47
cristian1525aragon <38145618+cristian1525aragon@users.noreply.github.com>
2020-03-04 16:53:54 -0500

    ejercicioColorDeResistencia

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+COLOR = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+]
+
+
+def color_code(color):
+    return COLOR.index(color)
+
+
+def colors():
+    return COLOR
